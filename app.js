8cf81c5464a8c40938d0f6637dc72331f4882701
an-kit13 <ankitkrsingh48@gmail.com>
2024-08-25 12:26:12 +0530

    netwoks configured

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const app = express();
+app.use(cors());
+app.use(express.json());
 const PORT = process.env.PORT || 8080;
 
 app.use(bodyParser.json());
